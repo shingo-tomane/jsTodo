c1211680d3d742b25b0096b1afc52d2e513d4328
shingo-tomane <142208852+shingo-tomane@users.noreply.github.com>
2023-08-17 20:25:43 +0900

    追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  createImcompleteList(inputText);
+};
+
+const deleteFromImcompleteList = (target) => {
+  document.getElementById("imcomplete-list").removeChild(target);
+};
+
+const createImcompleteList = (text) => {
+  document.getElementById("add-text").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = text;
+
+  const completebutton = document.createElement("button");
+  completebutton.innerText = "完了";
+  completebutton.addEventListener("click", () => {
+    deleteFromImcompleteList(completebutton.parentNode);
+
+    const addTarget = completebutton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    addTarget.textContent = null;
+
+    const li = document.createElement("li");
+    li.innerText = text;
+    const backbutton = document.createElement("button");
+    backbutton.innerText = "戻す";
+    backbutton.addEventListener("click", () => {
+      const deretetarget = backbutton.parentNode;
+      document.getElementById("complete-list").removeChild(deretetarget);
+
+      const text = backbutton.parentNode.firstElementChild.innerText;
+      createImcompleteList(text);
+    });
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backbutton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  const deletebutton = document.createElement("button");
+  deletebutton.innerText = "削除";
+  deletebutton.addEventListener("click", () => {
+    deleteFromImcompleteList(deletebutton.parentNode);
+  });
+
+  div.appendChild(li);
+  div.appendChild(completebutton);
+  div.appendChild(deletebutton);
+
+  document.getElementById("imcomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
